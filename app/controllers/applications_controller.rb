376fae00d102844f05856af4874e7b27e58b3e5b
Jamison Griffith <jamison.griffith@gmail.com>
2021-05-22 12:14:02 -0600

    Implement pet search on application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,6 +5,12 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
+
+    if params[:search].present?
+      @pets = Pet.search(params[:search])
+    else
+      @pets = Pet.adoptable
+    end
   end
 
   def new
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -44,4 +44,28 @@ RSpec.describe 'application info page' do
     expect(current_path).to eq "/pets/#{@barnaby.id}"
   end
 
+  it 'has a search functionality for pets' do
+    #[ ] done
+
+    #Searching for Pets for an Application
+
+    #As a visitor
+    #When I visit an application's show page
+    #And that application has not been submitted,
+    #Then I see a section on the page to "Add a Pet to this Application"
+    #In that section I see an input where I can search for Pets by name
+    #When I fill in this field with a Pet's name
+    #And I click submit,
+    #Then I am taken back to the application show page
+    #And under the search bar I see any Pet whose name matches my search
+    visit "/applications/#{@app1.id}"
+    within "section#add-a-pet" do
+    expect(page).to have_content("Add a Pet to this Application")
+    fill_in 'search', with: "Barnaby"
+    click_button 'Search'
+    expect(current_path).to eq "/applications/#{@app1.id}"
+    expect(page).to have_content @barnaby.name
+    expect(page).not_to have_content @sam.name
+    end
+  end
 end  
