af4a5470f28e22baed989531ccf72e386299f91b
Jamison Griffith <jamison.griffith@gmail.com>
2021-05-22 11:13:14 -0600

    Adds validations to application model

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Application do
 
     it { should have_many(:pets).through(:application_pets) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
+  end
 end
